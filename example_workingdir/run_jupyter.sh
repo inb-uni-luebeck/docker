ec5332c145ec220aa4643b45a47d4f75ee33cc11
Marius Jahrens <jahrens@inb.uni-luebeck.de>
2020-07-12 14:43:34 +0200

    fixed file permission problems for mounted volumes (adjusted run scripts: changed user and group id for the user inside the container to correspond to the current user)

--- example_workingdir/run_jupyter.sh
+++ example_workingdir/run_jupyter.sh
@@ -1,2 +1,2 @@
-docker run --gpus all --name dl --rm -p 8888:8888 -v $(pwd):/workingdir -v $HOME:/data dl_workingdir jupyter notebook --ip=0.0.0.0 --no-browser --allow-root
+docker run --gpus all --name dl --rm -p 8888:8888 -v $(pwd):/workingdir -v $HOME:/data --user $(id -u):$(id -g) dl_workingdir jupyter notebook --ip=0.0.0.0 --no-browser --allow-root
 
