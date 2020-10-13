14a505ad5607ae3df9e5b1bececfeb606629559b
Noah Heldt <heldt@pc08.inb.uni-luebeck.de>
2020-10-13 13:37:07 +0200

    fixed jupyter-startup-script

--- example_workingdir/run_jupyter.sh
+++ example_workingdir/run_jupyter.sh
@@ -1,2 +1,8 @@
-docker run --gpus all --name dl --rm -p 8888:8888 -v $(pwd):/workingdir -v $HOME:/data --user $(id -u):$(id -g) dl_workingdir jupyter notebook --ip=0.0.0.0 --no-browser --allow-root
-
+docker run \
+    --gpus all \
+    --name dl \
+    --rm -p 8888:8888 -v $(pwd):/workingdir -v /nfshome/heldt/Documents/data:/data \
+    pr jupyter notebook . \
+    --ip=0.0.0.0 \
+    --no-browser \
+    --allow-root 
